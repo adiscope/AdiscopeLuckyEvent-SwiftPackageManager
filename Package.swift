170c24b6a48d6689198c7a82538c436a09362025
kyungboshim <kyungbo.shim@neowiz.com>
2026-03-23 14:13:12 +0900

    5.2.3 배포

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdiscopeLuckyEvent",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.2/AdiscopeLuckyEvent.zip",
-            checksum: "6f64bd123651225b8a0f08c06d639c82128c75842d1eed5227e0896a69a955a4"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.3/AdiscopeLuckyEvent.zip",
+            checksum: "a68f92004e74737edf5e8394881ec8777b0aad899f7214902c8f3b76ef426c51"
         ),
     ]
 )
